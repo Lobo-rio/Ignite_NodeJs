4139c0d7c5c34229dc1af147f12fc4684d507e32
Marcos Vinícius <marcos.kichort@gmail.com>
2021-03-29 21:22:51 -0300

    Adicionando os arquivos do users

--- rentalx/src/modules/cars/useCases/createCategory/CreateCategoryController.ts
+++ rentalx/src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -10,7 +10,7 @@ class CreateCategoryController {
 
         const { name, description } = request.body;
 
-        this.createCategoryUseCase.execute({name, description})
+        this.createCategoryUseCase.execute({name, description});
 
         return response.status(201).send();
 
